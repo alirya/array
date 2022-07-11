13327f0bb1aea8724c6352ba6ce00c92effadedf
dikac <rigel234@yahoo.com>
2022-07-11 09:33:24 +0700

    ref: improve template type

--- src/boolean/equal.ts
+++ src/boolean/equal.ts
@@ -1,19 +1,22 @@
 import EqualBoolean from '@alirya/boolean/equal';
+import ValidationContainer from '@alirya/boolean/validation/validation';
+import Validation from '@alirya/boolean/function/validation';
+import {List} from 'ts-toolbelt';
 
-export function EqualParameters<Value>(
-    array1 : ReadonlyArray<Value>,
-    array2 : ReadonlyArray<Value>,
-    compare : (value1:Value, value2:Value) => boolean = EqualBoolean.Parameters
+export function EqualParameters<Value extends ReadonlyArray<unknown>>(
+    value : Value,
+    compare : Value,
+    validation : Validation<[List.UnionOf<Value>, List.UnionOf<Value>]> = EqualBoolean.Parameters
 ) : boolean {
 
-    if(array1.length !== array2.length) {
+    if(value.length !== compare.length) {
 
         return false;
     }
 
-    for(const [index, value] of array1.entries()) {
+    for(const [index, val] of value.entries()) {
 
-        if(!compare(value, array2[index])) {
+        if(!validation(val, compare[index])) {
 
             return false;
         }
@@ -22,25 +25,27 @@ export function EqualParameters<Value>(
     return true;
 }
 
-export type EqualArgument<Value> = {
-    list : [ReadonlyArray<Value>, ReadonlyArray<Value>],
-    compare ?: (value1:Value, value2:Value) => boolean
-};
+export type EqualArgument<Type extends ReadonlyArray<unknown>> =
+  EqualBoolean.Argument<Type>  & EqualArgumentValidation<Type>;
 
-export function EqualParameter<Value>(
-    {
-        list:[array1, array2],
-        compare
-    } : EqualArgument<Value>
+export type EqualArgumentValidation<Value  extends ReadonlyArray<unknown>> = Partial<ValidationContainer<[EqualBoolean.Argument<List.UnionOf<Value>>]>>;
+
+export function EqualParameter<Value extends ReadonlyArray<unknown>>(
+  {
+      value,
+      compare,
+      validation = EqualBoolean.Parameter
+  } : EqualArgument<Value>
 ) : boolean {
 
-    return EqualParameters(array1, array2, compare);
+    return EqualParameters(value, compare, (value, compare) => validation({value, compare}));
 }
 
 
 namespace Equal {
     export const Parameters = EqualParameters;
     export const Parameter = EqualParameter;
-    export type Argument<Value> = EqualArgument<Value>;
+    export type Argument<Value extends ReadonlyArray<unknown>> = EqualArgument<Value>;
+    export type ArgumentValidation<Value extends ReadonlyArray<unknown>> = EqualArgumentValidation<Value>;
 }
 export default Equal;
